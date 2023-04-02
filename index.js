56713472ae4ef9ca3585d0ccffacc578db73b2bb
iabbakr <abubakaribrahim603@gmail.com>
2023-04-02 15:48:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { menuArray } from './data.js';
 
 document.addEventListener('click', function(e){
     if(e.target.id === 'pizza-btn'){
-        handlePizzaClick();
+        handlePizzaClick(menuId);
     }else if(e.target.id === 'burger-btn'){
         handleBurgerClick();
     }else if(e.target.id === 'donut-btn'){
@@ -14,7 +14,10 @@ document.addEventListener('click', function(e){
 
 
 
-function handlePizzaClick(){
+function handlePizzaClick(menuId){
+    const targetMenuId = menuArray.filter(function(){
+        return menu.id = menuId;
+    })
 
     render();
     
@@ -40,32 +43,32 @@ function getCheckOutHtml(){
     menuArray.forEach(function(menu){
         
         checkOut += `
-    <div class="menu">
-                    <h3>Your order</h3>
-                    <div class="menu-check">
-                        <span class="">
-                        ${menu.name} 
-                        </span>
-                        <span class="">
-                        ${menu.price} 
-                        </span>
-                    </div>
-                    <div class="menu-bottom">
-                        <span class="">
-                        Total price:
-                        </span>
-                        <span class="" id="total-price">
-                        $
-                        </span>
-                    </div>
-                    <input type="button" 
-                    id="order-btn" 
-                    class="order-btn" 
-                    value="Complete order"/>
-                    
-                </div>
-    
-    `
+                        <div class="menu">
+                            <h3>Your order</h3>
+                            <div class="menu-check">
+                                            <span class="">
+                                            ${menu.name} 
+                                            </span>
+                                            <span class="">
+                                            ${menu.price} 
+                                            </span>
+                                        </div>
+                                        <div class="menu-bottom">
+                                            <span class="">
+                                            Total price:
+                                            </span>
+                                            <span class="" id="total-price">
+                                            $
+                                            </span>
+                                        </div>
+                                        <input type="button" 
+                                        id="order-btn" 
+                                        class="order-btn" 
+                                        value="Complete order"/>
+                                        
+                                    </div>
+                        
+                        `
     })
 };
 
